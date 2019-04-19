16d8694d769698a7d2bf9bea8b126ad6e5c2126e
jpaav <joepaavola@gmail.com>
2019-04-19 11:50:57 -0400

    more heroku stuff

--- chs_weather/settings.py
+++ chs_weather/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     # 'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PERMISSION_CLASSES': (
